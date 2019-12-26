7cfaeb7b379c66676469cce79ef3fb47064712b9
alex629lee <alex629lee@yahoo.com>
2019-12-25 21:22:38 -0800

    Add modal to photo thumbnails

--- client/src/components/thumbnails/ThumbIndexItem.jsx
+++ client/src/components/thumbnails/ThumbIndexItem.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
+import { withRouter } from "react-router-dom";
 
-export default class ThumbIndexItem extends React.Component {
+class ThumbIndexItem extends React.Component {
   render() {
     const { thumb } = this.props;
+
     return (
       <li className="thumb-index-item">
-        <img src={thumb.photoUrl} />
+        <img src={thumb.photoUrl} onClick={() => this.props.history.push(`/photos/${thumb._id}`)} />
 
-        <div>
-          <p>{thumb.likes.length} Likes</p>
-          <p>{thumb.comments.length} Comments</p>
+        <div className="thumb-index-item-modal" onClick={() => this.props.history.push(`/photos/${thumb._id}`)}>
+          <p><i className="fas fa-music"></i> {thumb.likes.length}</p>
+          <p><i className="fas fa-comment"></i> {thumb.comments.length}</p>
         </div>
       </li>
     )
   }
 }
+
+export default withRouter(ThumbIndexItem);
