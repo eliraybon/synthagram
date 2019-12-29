d210bb1035b65136f0859caa0c1d7e02d098eebd
eliraybon <elr0615@gmail.com>
2019-12-28 20:11:20 -0500

    Update production keys

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,6 @@
 module.exports = {
   MONGO_URI: process.env.MONGO_URI,
   secretOrKey: process.env.SECRET_OR_KEY,
+  awsAccessKey: process.env.AWS_ACCESS_KEY,
+  awsSecretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 }
